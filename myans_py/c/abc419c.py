d7bee3037aa38feab1794fdf10ef4d1a7198716a
smitsu32 <33113176@stn.nitech.ac.jp>
2026-08-11 00:06:57 +0900

    abc419c.pyを更新

--- myans_py/c/abc419c.py
+++ myans_py/c/abc419c.py
@@ -1,13 +1,9 @@
-n=int(input())
-INF=10**18
+s=input()
+e=['red','blue','green']
+a=['SSS','FFF','MMM']
 
-#同時に動けるから一番遠いのを独立に考える
-mxx,mnx,mxy,mny=-INF,INF,-INF,INF
-for i in range(n):
-    x,y=map(int, input().split())
-    mxx=max(mxx,x)
-    mnx=min(mnx,x)
-    mxy=max(mxy,y)
-    mny=min(mny,y)
-
-print(max((mxx-mnx+1)//2,(mxy-mny+1)//2))
+for i in range(3):
+    if e[i]==s:
+        print(a[i])
+        exit()
+print('Unknown')
